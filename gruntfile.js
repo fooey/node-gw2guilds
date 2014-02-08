87c6976e8fb9b17293a1f9d2300bb92c3325402c
Jason Rushton <github.com@schtuph.com>
2014-02-08 16:03:34 -0700

    enable liveupdate module via Grunt

--- gruntfile.js
+++ gruntfile.js
@@ -1,11 +1,22 @@
 module.exports = function(grunt) {
 
+	grunt.loadNpmTasks('grunt-concurrent');
 	grunt.loadNpmTasks('grunt-nodemon');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
 
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+
+		concurrent: {
+			target: {
+				tasks: ['nodemon', 'watch'],
+				options: {
+					logConcurrentOutput: true
+				}
+			}
+		},
 
 		nodemon: {
 			dev: {
@@ -15,15 +26,36 @@ module.exports = function(grunt) {
 					watchedExtensions: ['js', 'jade', 'json'],
 					delayTime: 1,
 					env: {
-						PORT: '3000',
+						PORT: '3001',
 						NODE_ENV: 'development'
 					}
 				}
 			}
 		},
+
+		watch: {
+			css: {
+				files: 'public/css/style.css',
+				options: {
+					livereload: true, 
+					interval: 500,
+					debounceDelay: 500,
+				},
+			},
+			appJs: {
+				files: [
+					'public/js/script.js'
+				],
+				options: {
+					livereload: true,
+					interval: 500,
+					debounceDelay: 500,
+				},
+			},
+		},
 	});
 
 
 
-	grunt.registerTask('dev', ['nodemon']);
+	grunt.registerTask('dev', ['concurrent']);
 };

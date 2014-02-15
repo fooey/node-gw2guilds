133cf462c928369c8d463c41db6a4df51d45b1e6
Jason Rushton <github.com@schtuph.com>
2014-02-15 16:34:42 -0700

    enable minimal http logging on dev

--- config/server.js
+++ config/server.js
@@ -1,10 +1,12 @@
+"use strict"
+
+
 module.exports = function (app, express) {
 	const config = this;
 
 	const path = require('path')
 	const pubFolder = path.join(process.cwd(), 'public');
 	const faviconPath = path.join(pubFolder, 'images/gw2-dragon-32.png');
-
 
 
 	if(process.env.NODE_ENV === 'development'){
@@ -16,6 +18,24 @@ module.exports = function (app, express) {
 		app.use(express.errorHandler());
 		app.use(express.logger('default'));
 	}
+
+	
+	app.use(express.cookieParser());
+
+	// set a cookie
+	app.use(function (req, res, next) {
+
+		let uaUUID = req.cookies.uaUUID;
+
+		if(!uaUUID){
+	    	uaUUID = require('uuid').v4();
+
+			const cookieMaxAge = 1000 * 60 * 60 * 24 * 356 * 2; // 2 years
+			res.cookie('uaUUID', uaUUID, { maxAge: cookieMaxAge, httpOnly: true});
+		}
+
+		next(); // <-- important!
+	});
 
 	app.set('port', process.env.PORT || 3000);
 

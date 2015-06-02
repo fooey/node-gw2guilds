cd17b6246c3c3b9871facc9573fa614b0c7c3f68
Jason Rushton <github.com@schtuph.com>
2015-06-02 17:01:28 -0600

    change contact link to be an email address

--- public/js/dist/app.js
+++ public/js/dist/app.js
@@ -20,15 +20,37 @@ module.exports = {
 
 $(function () {
 
-	var $searchForm = $('form.guildSearch');
-	if ($searchForm && $searchForm.length) {
-		require('./search.js')($searchForm);
-	}
+    var $searchForm = $('form.guildSearch');
+    if ($searchForm && $searchForm.length) {
+        require('./search.js')($searchForm);
+    }
 
-	var $linkBuilder = $('#linkBuilder');
-	if ($linkBuilder && $linkBuilder.length) {
-		require('./linkBuilder.js')($linkBuilder);
-	}
+    var $linkBuilder = $('#linkBuilder');
+    if ($linkBuilder && $linkBuilder.length) {
+        require('./linkBuilder.js')($linkBuilder);
+    }
+
+    (function nospam(selector) {
+        var addr = getAddr();
+        var newEl = console.log(selector, addr);
+
+        $(selector).each(function (i, el) {
+            var $el = $(el);
+            console.log($el);
+
+            $el.replaceWith($('<a>', {
+                href: 'mailto:' + addr,
+                html: addr
+            }));
+        });
+    })('.contact');
+
+    function getAddr() {
+        var src = '@ gw2w2w schtuph com .'.split(' ');
+        var addr = [src[1], src[0], src[2], src[4], src[3]].join('');
+
+        return addr;
+    }
 });
 
 },{"./linkBuilder.js":3,"./search.js":4}],3:[function(require,module,exports){
--- public/js/src/app.js
+++ public/js/src/app.js
@@ -13,4 +13,30 @@ $(function() {
 		require('./linkBuilder.js')($linkBuilder);
 	}
 
+
+
+
+    (function nospam(selector) {
+        var addr = getAddr();
+        var newEl =
+        console.log(selector, addr);
+
+        $(selector).each(function(i, el) {
+            var $el = $(el);
+            console.log($el);
+
+            $el.replaceWith($('<a>', {
+                href: 'mailto:' + addr,
+                html: addr
+            }));
+        });
+    }('.contact'));
+
+    function getAddr() {
+        var src = '@ gw2w2w schtuph com .'.split(' ');
+        var addr = [src[1], src[0], src[2], src[4], src[3]].join('');
+
+        return addr;
+    }
+
 });

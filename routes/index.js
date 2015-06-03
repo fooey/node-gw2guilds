84647d8a6de0aa6fa341751539aeb812c12bd14f
Jason Rushton <github.com@schtuph.com>
2015-06-02 18:13:10 -0600

    tweak the way guilds/index.json is created and loaded

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,23 @@
 "use strict";
 
+const DB = require('../lib/data');
+
 
 module.exports = function(app, express) {
 	var routes = this;
 
 	if (process.env.NODE_ENV === 'development') {
 		app.get('/stressTest', require('./stressTest.js'));
-
-		app.get('/dump', function(req, res) {
-			const DB = require('../lib/data');
-			res.json(DB.guilds.index.toJS());
-		});
 	}
 
+
 	app.get('/', require('./home.js'));
+
+
 	// /0D8B74FC-F940-4314-8EAF-8F8C52C4C4EB
 	app.get('/:guildId([A-Z0-9-]{36}).:size([0-9]+)?.:extension(svg)?', require('./shortlink.js'));
 	app.get('/:guildId([A-Z0-9-]{36}).:size([0-9]+)?.:bgColor([a-z0-9]+)?.:extension(svg)?', require('./shortlink.js'));
+
 
 	app.get('/guilds/:guildSlug/:size([0-9]+).:bgColor([a-z0-9]+)?.svg', require('./emblem.js'));
 	// app.get('/guilds/:guildSlug/:size([0-9]+).svg', require('./emblem.js'));
@@ -26,6 +27,10 @@ module.exports = function(app, express) {
 
 	app.get('/robots.txt', require('./robots.js'));
 	app.get('/sitemap.xml', require('./sitemap.js'));
+
+	app.get('/dump', function(req, res) {
+		res.json(DB.guilds.index.toJS());
+	});
 
 	return routes;
 };

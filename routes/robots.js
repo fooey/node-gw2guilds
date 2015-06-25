4a2bceaa70520b7bb5f66e7695e513cd80eed454
Jason Rushton <github.com@schtuph.com>
2015-06-24 22:32:08 -0600

    don't block *.svg

--- routes/robots.js
+++ routes/robots.js
@@ -5,13 +5,13 @@
 module.exports = function(req, res) {
     // const renderStart = Date.now()
 
-    res.header('content-type', 'text/plain');
-    res.end([
+    // res.header('content-type', 'text/plain');
+    res.type('text').send([
         '#' + req.headers.host,
         '',
-        'User-agent: *',
-        'Disallow: *.svg$',
-        '',
+        // 'User-agent: *',
+        // 'Disallow: *.svg$',
+        // '',
         'Sitemap: http://' + req.headers.host + '/sitemap.xml',
     ].join('\n'));
 };

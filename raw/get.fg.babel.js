ace852eecae417dab3953f829152601a5fffb9b7
Jason Rushton <github.com@schtuph.com>
2015-11-20 16:19:12 -0700

    scripts to download the new emblem data

---
+++ raw/get.fg.babel.js
@@ -0,0 +1,56 @@
+import path from 'path';
+import fs from 'fs-extra';
+
+import async from 'async';
+import _ from 'lodash';
+import request from 'request';
+
+
+/*
+*   module globals
+*/
+
+const localRoot = './fg';
+
+
+let dlQueue = async.queue(function (task, cb) {
+    console.log('download %s to %s', task.remotePath, task.localPath);
+    downloadFile(task.remotePath, task.localPath, cb);
+}, 8);
+
+
+/*
+*
+*   MAIN
+*
+*/
+
+request(
+    'https://api.guildwars2.com/v2/emblem/foregrounds?ids=all',
+    (err, response, body) => {
+        const data = JSON.parse(body);
+
+        _.each(data, (fg) => {
+            _.each(fg.layers, (remotePath, layerIndex) => {
+                const localPath = path.resolve(localRoot, fg.id.toString(), `${layerIndex}.png`);
+                // console.log(localRoot, fg.id, layerIndex, localPath);
+                dlQueue.push({
+                    remotePath,
+                    localPath,
+                });
+            });
+        });
+    }
+);
+
+
+
+function downloadFile(remotePath, localPath, cb) {
+    request(remotePath)
+        .pipe(fs.createOutputStream(localPath))
+        .on('finish', () => {
+            console.log(`downloaded %s`, remotePath);
+            cb();
+        });
+
+}

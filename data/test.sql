c9ecd76967c5954ad38e4cd3a7ddd6a3d26c910f
Jason Rushton <github@schtuph.com>
2016-07-11 22:59:25 -0600

    moar tests

--- data/test.sql
+++ data/test.sql
@@ -4,14 +4,32 @@ order by date_created desc;
 
 truncate table guilds;
 
+--                 explain 
                 SELECT *
                 FROM guilds
-                WHERE slug = 'ring-of-fire-community-guild';
+                WHERE slug = 'arenanet';
                 
 --                 explain 
                 SELECT *
                 FROM guilds
                 WHERE guild_id = '55C83169-5053-E511-A5A9-AC162DAE5A05';
 
+                SELECT *
+                FROM guilds
+                order by modified_date desc
+                LIMIT 100;
 
-                SELECT CAST(EXTRACT(EPOCH FROM Now()) as INT);
+--                 explain 
+                SELECT *
+                FROM guilds
+		TABLESAMPLE BERNOULLI(1)
+                WHERE background_id IS NOT NULL
+			AND modified_date >= 1467525312 - (1000 * 60 * 60)
+                LIMIT 5;
+
+
+select (SELECT count(*) FROM guilds),
+	(SELECT reltuples AS approximate_row_count FROM pg_class WHERE relname = 'guilds');
+
+	analyze;
+	vacuum;

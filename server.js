42a81d7dab422fd138322cd1ba3458a62ede2f91
Jason Rushton <github@schtuph.com>
2018-05-10 20:58:12 -0600

    swap in sentry in place of newrelic

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 require("babel-register");
 require('dotenv').load();
 
-if (process.env.NODE_ENV === 'production') {
-    console.log('enabling newrelic service');
-    require('newrelic');
-}
+const Raven = require('raven');
+Raven.config('https://8136d808c63f410ba55548b92876cfdd@sentry.io/1204811').install();
+
+// if (process.env.NODE_ENV === 'production') {
+//     console.log('enabling newrelic service');
+//     require('newrelic');
+// }
 
 
 process.chdir(__dirname);

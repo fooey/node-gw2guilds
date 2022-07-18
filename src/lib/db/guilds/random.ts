fd792b018465c1a903002213d9e18fcc3384f7af
Jason Rushton <jason@jasonrushton.com>
2022-07-17 20:38:59 -0600

    add luxon

--- src/lib/db/guilds/random.ts
+++ src/lib/db/guilds/random.ts
@@ -1,3 +1,4 @@
+import { DateTime } from 'luxon';
 import { db } from '~/lib/db/db';
 import { IGuild } from '~/types/Guild';
 
@@ -9,7 +10,7 @@ const sqlStatement = db.prepare(`
     FROM guilds
     WHERE background_id IS NOT NULL
       AND foreground_id IS NOT NULL
-      AND modified_date > date('2022-01-01')
+      AND modified_date > date(@date)
     ORDER BY RANDOM()
     LIMIT @limit
   );
@@ -19,8 +20,9 @@ export const DEFAULT_RANDOM_GUILDS_NUM = 20;
 const MAX_LIMIT = 100;
 
 export const lookupRandomGuilds = (num: number = DEFAULT_RANDOM_GUILDS_NUM): IGuild[] => {
+  const date = DateTime.utc().minus({ days: 30 });
   const limit = Math.min(num, MAX_LIMIT);
-  const guilds: IGuild[] = sqlStatement.all({ limit });
+  const guilds: IGuild[] = sqlStatement.all({ limit, date: date.toISODate() });
 
   return guilds;
 };

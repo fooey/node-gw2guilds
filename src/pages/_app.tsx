446b342800dc86fff0f6758a9b0384ae0e45c5d6
Jason Rushton <jason@jasonrushton.com>
2022-07-24 22:09:08 -0600

    add minimal logging

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
+import { DateTime } from 'luxon';
 import type { AppProps } from 'next/app';
 import '~/styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
+  console.info(DateTime.now().toISO(), router.pathname, router.query);
   return <Component {...pageProps} />;
 }
 

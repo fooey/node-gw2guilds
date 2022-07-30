37fb991961129bcef7d65325b65432a0a5a86e2e
Jason Rushton <jason@jasonrushton.com>
2022-07-30 16:16:26 -0600

    better color UX

--- src/components/EmblemBuilder/colors.tsx
+++ src/components/EmblemBuilder/colors.tsx
@@ -1,6 +1,7 @@
 import classnames from 'classnames';
-import { every, includes, map } from 'lodash';
-import { useState } from 'react';
+import { every, includes, intersection, map, reject } from 'lodash';
+import React, { useState } from 'react';
+import { MdClear, MdWarning } from 'react-icons/md';
 import { EMBLEM_SWATCH_SIZE } from '~/lib/emblem/constants';
 import { colors, IColor } from '~/lib/emblem/resources';
 import { IGuildEmblem } from '~/types/Guild';
@@ -12,6 +13,9 @@ const categoryTypes = {
   Material: ['Vibrant', 'Metal', 'Leather'],
   Color: ['Gray', 'Blue', 'Orange', 'Brown', 'Red', 'Purple', 'Green', 'Yellow'].sort(),
 };
+
+type Category = string;
+type CategoryTypes = keyof typeof categoryTypes;
 
 export interface IColorSelectionProps {
   title: string;
@@ -66,14 +70,18 @@ export const ColorPicker: React.FC<IColorPickerProps> = ({
   onChange,
   onClose,
 }) => {
-  const [categoryFilters, setCategoryFilters] = useState<string[]>([]);
+  const [categoryFilters, setCategoryFilters] = useState<string[]>(['Starter']);
 
-  const handleFilterClick = (category: string) => {
+  const handleFilterClick = (newState: boolean | null, category: Category | null, categoryType: CategoryTypes) => {
+    const typeCategories = categoryTypes[categoryType];
+
     return setCategoryFilters((currentState) => {
-      if (currentState.includes(category)) {
-        return currentState.filter((c) => c !== category);
-      } else {
+      currentState = reject(currentState, (c) => typeCategories.includes(c));
+
+      if (newState && category) {
         return [...currentState, category];
+      } else {
+        return currentState;
       }
     });
   };
@@ -121,25 +129,42 @@ export const ColorPicker: React.FC<IColorPickerProps> = ({
 };
 
 interface IColorFiltersProps {
-  categoryFilters: string[];
-  onSelect: (category: string) => void;
+  categoryFilters: Category[];
+  onSelect: (newState: boolean | null, category: Category | null, categoryType: CategoryTypes) => void;
 }
 const ColorFilters: React.FC<IColorFiltersProps> = ({ categoryFilters, onSelect }) => {
   return (
     <div className="mx-auto flex w-fit flex-row flex-wrap gap-8 pt-2">
-      {map(categoryTypes, (v, k) => {
+      {map(categoryTypes, (typeCategories: Category[], categoryType: CategoryTypes) => {
+        const hasActive = intersection(typeCategories, categoryFilters).length > 0;
+
         return (
-          <div key={k} className="flex flex-row gap-2">
-            {v.map((category) => {
+          <div key={categoryType} className="flex flex-row gap-2">
+            {hasActive ? (
+              <MdClear
+                className="cursor-pointer text-neutral-300"
+                title={`Clear ${categoryType}`}
+                onClick={() => onSelect(null, null, categoryType)}
+              />
+            ) : null}
+            {typeCategories.map((category) => {
               const isActive = categoryFilters.includes(category);
               const filterClassName = classnames({
                 'font-bold  ': isActive && categoryFilters.length !== 0,
               });
 
               return (
-                <button key={category} className={` text-xs  ${filterClassName}`} onClick={() => onSelect(category)}>
-                  {category}
-                </button>
+                <React.Fragment key={category}>
+                  {category === 'Starter' && isActive === false ? (
+                    <MdWarning className="text-red-800" title="Only starter colors are available in game" />
+                  ) : null}
+                  <button
+                    className={` text-xs  ${filterClassName}`}
+                    onClick={() => onSelect(!isActive, category, categoryType)}
+                  >
+                    {category}
+                  </button>
+                </React.Fragment>
               );
             })}
           </div>

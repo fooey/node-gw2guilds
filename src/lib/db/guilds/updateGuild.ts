f06b004d39c04492aedfc87017659b8c6043fd94
Jason Rushton <jason@jasonrushton.com>
2022-07-30 16:53:03 -0600

    better modified vs checked data

--- src/lib/db/guilds/updateGuild.ts
+++ src/lib/db/guilds/updateGuild.ts
@@ -1,8 +1,16 @@
+import { some } from 'lodash';
 import { DateTime } from 'luxon';
 import { IGuild } from '~/types/Guild';
 import { db } from '../db';
+import { selectGuildById } from './guildById';
 
-const updateStatement = db.prepare(`
+const updateCheckedStatement = db.prepare(`
+  UPDATE guilds
+  SET checked_date = @now
+  WHERE guild_id LIKE @guild_id
+`);
+
+const updateModifiedStatement = db.prepare(`
   UPDATE guilds SET
     guild_name = @guild_name
     , slug = @slug
@@ -24,16 +32,48 @@ const updateStatement = db.prepare(`
 
 export const updateGuild = (guild: IGuild) => {
   const now = DateTime.utc().set({ millisecond: 0 }).toISO({ suppressMilliseconds: true });
+  const guild_id = guild.guild_id.toLowerCase();
 
   const bindable = {
     ...guild,
-    guild_id: guild.guild_id.toLowerCase(),
     now,
+    guild_id,
     flags_flip_bg_horizontal: guild.flags_flip_bg_horizontal ? 1 : 0,
     flags_flip_bg_vertical: guild.flags_flip_bg_vertical ? 1 : 0,
     flags_flip_fg_horizontal: guild.flags_flip_fg_horizontal ? 1 : 0,
     flags_flip_fg_vertical: guild.flags_flip_fg_vertical ? 1 : 0,
   };
 
-  return updateStatement.run(bindable);
+  if (checkIfModified(bindable)) {
+    return updateModifiedStatement.run({ ...bindable, now });
+  } else {
+    return updateCheckedStatement.run({ guild_id, now });
+  }
+};
+const checkFields = [
+  `guild_name`,
+  `slug`,
+  `tag`,
+  `background_id`,
+  `foreground_id`,
+  `background_color_id`,
+  `foreground_primary_color_id`,
+  `foreground_secondary_color_id`,
+  `flags_flip_bg_horizontal`,
+  `flags_flip_bg_vertical`,
+  `flags_flip_fg_horizontal`,
+  `flags_flip_fg_vertical`,
+];
+
+const checkIfModified = (guild: any) => {
+  const dbGuild = selectGuildById(guild.guild_id)!;
+
+  const isModified = some(checkFields, (key: string) => {
+    const currentValue = dbGuild[key as keyof IGuild];
+    const newValue = guild[key];
+
+    return currentValue !== newValue;
+  });
+
+  return isModified;
 };

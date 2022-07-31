590ec58ca4988b87258b075f07c787d7fc1e35cc
Jason Rushton <jason@jasonrushton.com>
2022-07-30 21:50:31 -0600

    ignore linting for faster production builds

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
   images: {
     dangerouslyAllowSVG: true,

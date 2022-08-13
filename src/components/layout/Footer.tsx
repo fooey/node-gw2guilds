476f788b0ba6bbcfe958314346683a968abbb37e
Jason Rushton <jason@jasonrushton.com>
2022-08-13 16:01:44 -0600

    add thanks and credit for mtodor

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,7 +4,7 @@ import { MdCopyright, MdEmail } from 'react-icons/md';
 export const Footer = () => {
   return (
     <footer className="mt-20 w-full flex-none bg-slate-100">
-      <div className="mx-auto my-8 flex max-w-3xl flex-col place-content-center gap-2 text-center text-xs text-slate-500">
+      <div className="mx-auto my-10 flex max-w-3xl flex-col place-content-center gap-4 text-center text-xs text-slate-500">
         <div className="flex flex-row items-center justify-center">
           <div className="flex w-64 flex-col items-center justify-center">
             <MdEmail className="text-6xl" />
@@ -25,12 +25,15 @@ export const Footer = () => {
             </p>
           </div>
         </div>
+        <p>
+          <MdCopyright className="inline p-0" /> 2013 ArenaNet, Inc. All rights reserved. NCsoft, the interlocking NC
+          logo, ArenaNet, Guild Wars, Guild Wars Factions, Guild Wars Nightfall, Guild Wars: Eye of the North, Guild
+          Wars 2, and all associated logos and designs are trademarks or registered trademarks of NCsoft Corporation.
+          All other trademarks are the property of their respective owners.
+        </p>
 
         <p>
-          <MdCopyright className="relative -top-1 m-0 inline p-0" /> 2013 ArenaNet, Inc. All rights reserved. NCsoft,
-          the interlocking NC logo, ArenaNet, Guild Wars, Guild Wars Factions, Guild Wars Nightfall, Guild Wars: Eye of
-          the North, Guild Wars 2, and all associated logos and designs are trademarks or registered trademarks of
-          NCsoft Corporation. All other trademarks are the property of their respective owners.
+          Special thanks to <a href="https://github.com/mtodor/gw2emblem">mtodor</a> for creating the SVG paths
         </p>
       </div>
     </footer>
